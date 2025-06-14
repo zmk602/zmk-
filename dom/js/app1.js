ddd5826b8294c78085f32a0e44bc6734a8ce876b
zmk602 <506349855@qq.com>
2025-06-14 22:42:59 +0800

    Add files via upload

---
+++ dom/js/app1.js
@@ -0,0 +1,67 @@
+//使用const声明变量
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+const statusElement = document.getElementById('status');
+const sortByNameBtn = document.getElementById('sortByName');
+const sortByDateBtn = document.getElementById('sortByDate');
+let tasks = [
+    {
+        text:"fff4",createdAt: new Date(2025, 2, 14),
+    },{
+        text:"7767764",createdAt: new Date(2025, 1, 16),
+    }
+]
+ 
+function update() {
+    taskList.innerHTML = '';
+    tasks.forEach(task => {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.className = 'task-text';
+        span.textContent = task.text;
+
+        const dateSpan = document.createElement('span');
+        dateSpan.className = 'task-date';
+        dateSpan.textContent = "(" + task.createdAt.toLocaleDateString() +")";
+        li.appendChild(span);
+        li.appendChild(dateSpan);
+        taskList.appendChild(li);
+    })
+    statusElement.innerHTML = `当前总任务数:  {tasks.length}`;
+}
+
+function addTask() {
+    if(!taskInput.value) {
+     alert('Please enter a task');
+     return;
+    }
+    const newtask = {
+     text: taskInput.value,
+     createdAt: new Date(),
+    };
+    tasks.push(newtask);
+    taskInput.value = '';
+    update()
+ }
+ addBtn.addEventListener('click', addTask);
+ taskInput.addEventListener('keypress', (event) => {
+    if(event.key === 'Enter') {
+        addTask();
+    }
+ })
+
+ function sortTasksByName() {
+    tasks.sort((a, b) => a. text.localeCompare(b.text));
+    update();
+ }
+
+ function sortTasksByDate() {
+    tasks.sort((a, b) => a.createdAt - b.createdAt);
+    update();
+ }
+
+ sortByNameBtn.addEventListener('click', sortTasksByName);
+ sortByDateBtn.addEventListener('click', sortTasksByDate);
+ update();
+ 
---
+++ dom/js/greeting.js
@@ -0,0 +1,7 @@
+export default function sayHello(name) {
+  return `你好, ${name}!`;
+}
+
+export function sayGoodbye(name) {
+  return `再见, ${name}!`;
+}
